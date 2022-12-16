9223c7961497cdf5fb3f1ba3a307a0989e1525dd
Raihan Uddin <raihan165221@gmail.com>
2022-12-16 18:55:02 +0600

    slider aded

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className='banner'>
             <div className="banner-text">
                 <h3 className='banner-heading'>Welcome</h3>
-                <h1 className='banner-title'>I'M A Professional Web Designer.</h1>
+                <h1 className='banner-title'>I'M A Professional Web Developer.</h1>
                 <p className='banner-description'>I am an experienced and expert web developer with having 1 year of experience.</p>
                 <button className='resume-btn'>DOWNLOAD RESUME</button><br />
                 <div className='animation-area'>
--- src/components/Claint/Claint.js
+++ src/components/Claint/Claint.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
-const Claint = () => {
+import './Claint.css';
+const Claint = ({ claint }) => {
+    const { name, img, said, rating } = claint;
     return (
-        <div>
-            sfd
+        <div className='single-claint'>
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>{said}</p>
+            <span>Rating: {rating}</span>
         </div>
     );
 };
